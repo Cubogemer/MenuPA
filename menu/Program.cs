adebe7e48807dd0ced46bfc15c8501785b4888cd
Cubogemer <119136869+Cubogemer@users.noreply.github.com>
2023-12-10 19:00:46 -0600

    Subo el projecto completo para descarga
    El archivo a modificar es "/menu/Program.cs"

---
+++ menu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Menu";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            Console.WriteLine("Este es el menu para mostrar los documentos de proyecto aula");
+            Console.ReadKey();
+            Console.Clear();
+
+             
+
+
+
+
+        }
+    }
+}
