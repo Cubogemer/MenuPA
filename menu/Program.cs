860f72dd7cb930cf9ada93985ae89cf5da70e0b2
Cubogemer <119136869+Cubogemer@users.noreply.github.com>
2023-12-16 16:46:40 -0600

    Update Program.cs
    Se fueron los comentarios, sigo trabajando en eso

--- menu/Program.cs
+++ menu/Program.cs
@@ -7,21 +7,38 @@ namespace menu
     {
         static void Main(string[] args)
         {
-            //Declaro variables 
             int opcion;
-            bool  endProgram = true;
+            bool endProgram = true;
             string line;
 
-            //Actualizo el nombre y los colores de la terminal
             Console.Title = "Menu";
             Console.BackgroundColor = ConsoleColor.DarkMagenta;
-            Console.ForegroundColor = ConsoleColor.White ;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
 
-            //Escribo la primera linea de texto
-            Console.WriteLine("Este es el menu para mostrar los documentos de proyecto aula");
-            Console.ReadKey();
-            Console.Clear();
+            try
+            {
+                StreamReader sr = new StreamReader("licensia.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exeption: " + e.Message);
+                Thread.Sleep(350);
+                Console.Clear();
+            }
+            finally
+            {
+                Console.ReadKey();
+                Console.Clear();
+            }
 
             while (endProgram)
             {
@@ -32,6 +49,7 @@ namespace menu
                     Console.WriteLine("2: Opcion 2");
                     Console.WriteLine("3: Salir del programa");
                     opcion = int.Parse(Console.ReadLine());
+
                     switch (opcion)
                     {
                         case 1:
@@ -60,10 +78,12 @@ namespace menu
                             }
                             Console.Clear();
                             break;
+
                         case 2:
                             Console.Clear ();
                             Console.WriteLine("Escogiste la opcion 2");
                             break;
+
                         case 3:
                             Console.Clear ();
                             Console.WriteLine("Has elegido salir del programa");
@@ -71,6 +91,7 @@ namespace menu
 
                             endProgram = false;
                             break;
+
                         default:
                             Console.WriteLine("No escribiste un numero dentro del rango");
                             break;
@@ -84,30 +105,6 @@ namespace menu
 
                 
             }
-
-
-
-        }
-    }
-}﻿using System;
-
-namespace menu
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Title = "Menu";
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-
-            Console.WriteLine("Este es el menu para mostrar los documentos de proyecto aula");
-            Console.ReadKey();
-            Console.Clear();
-
-             
-
 
 
 
